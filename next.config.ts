bb6407c62976662b846dadf48c802ce84ab39a94
DESKTOP-07RRC1V\Alex <227007373@stu.vtc.edu.hk>
2025-12-07 22:45:44 +0800

    Update next config file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const repoPath = '/cangjie-practice';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
   basePath: repoPath,
